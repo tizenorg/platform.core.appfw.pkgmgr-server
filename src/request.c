78b05f9f121346506e2dc0c00ada9ba3442d2eae
Junghyun Yeon <jungh.yeon@samsung.com>
2016-07-07 19:14:14 +0900

    Implement pkg enable/disable
    Change-Id: I2ebab9538ada5468b0a9859ae171dfdf740fda2c
    Signed-off-by: Junghyun Yeon <jungh.yeon@samsung.com>

--- src/request.c
+++ src/request.c
@@ -553,6 +553,7 @@ static int __handle_request_disable_pkg(uid_t uid,
 	uid_t target_uid = (uid_t)-1;
 	char *pkgtype = NULL;
 	char *pkgid = NULL;
+	char *reqkey = NULL;
 
 	g_variant_get(parameters, "(u&s&s)", &target_uid, &pkgtype, &pkgid);
 	if (target_uid == (uid_t)-1 || pkgid == NULL) {
@@ -561,15 +562,21 @@ static int __handle_request_disable_pkg(uid_t uid,
 		return -1;
 	}
 
+	reqkey = __generate_reqkey(pkgid);
+	if (reqkey == NULL)
+		return -1;
+
 	if (_pm_queue_push(target_uid, "", PKGMGR_REQUEST_TYPE_DISABLE_PKG, pkgtype,
 				pkgid, "")) {
 		g_dbus_method_invocation_return_value(invocation,
-				g_variant_new("(i)", PKGMGR_R_ESYSTEM));
+				g_variant_new("(is)", PKGMGR_R_ESYSTEM, ""));
+		free(reqkey);
 		return -1;
 	}
 
 	g_dbus_method_invocation_return_value(invocation,
-			g_variant_new("(i)", PKGMGR_R_OK));
+			g_variant_new("(is)", PKGMGR_R_OK, reqkey));
+	free(reqkey);
 
 	return 0;
 }
